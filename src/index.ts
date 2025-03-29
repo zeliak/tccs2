53df242f6a6e5c28db18da8e22ea16c0b30fd3ec
zeliak <kis.oliver@outlook.hu>
2025-03-29 20:15:22 +0100

    added global badges fetch

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,12 @@ if (null === clientSecret) {
 }*/
 
 app.use(express.json());
+app.use(function(req: Request, res: Response, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
+  next();
+});
 app.use('/twitch', twitchRoutes);
 
 app.get('/status', (req: Request, res: Response) => {
--- src/routes/twitch.ts
+++ src/routes/twitch.ts
@@ -1,54 +1,69 @@
 import { Router, Request, Response } from "express";
 import tApi from "../lib/apiConnection"
+import { twitchGlobalBadges } from "../lib/badges"
 
 const router = Router();
+const  corsHeader = new Headers()
+corsHeader.append('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-
-router.post('/', (req: Request, res: Response) => {
-    let userName = req.query.userName || null;
-    console.log('Username is: ' + userName)
-
-
-    if (!!userName) {
-        res.status(201).json({
-            "userName":userName,
-            "id":1234
-        });
-    }
-    else {
-        res.status(400).json({
-            "err":"Request does not contain a username"
-        });
-    }
-});
-
-router.get('/', (req: Request, res: Response) => {
-    res.json({
-        "AppName":"TCCS2"
-    });
-});
 
 router.get('/userpfp', (req: Request, res: Response) => {
-    let header = new Headers()
-    header.append('Access-Control-Allow-Origin', 'http://localhost:3000');
-
     const paramName = 'username';
+
     if (!!req.query && undefined !== req.query[paramName]) {
         console.log(req.query[paramName]);
         tApi.apiConnection.users.getUserByName(req.query[paramName] as string)
         .then(user => {
-            res.status(200).setHeaders(header).json({
+            res.status(200).json({
                 userName:user?.displayName,
                 pfpUrl:user?.profilePictureUrl
             });
         })
         .catch(e => {
             console.error(e);
-            res.status(500).setHeaders(header).send('Internal Server Error')
+            res.status(500).send('Internal Server Error')
         })
     }
     else {
-        res.status(400).send("Bad Request")
+        res.status(400).send("Bad Request");
+    }
+});
+
+interface IUserBadge {
+    name: string
+    imgUrl?: string
+    version: string
+}
+
+router.post('/userbadge', async (req: Request, res: Response) => {
+    const data = req.body;
+    var resolvedBadges: IUserBadge[] = [];
+    const ge = await twitchGlobalBadges;
+
+    for (let i = 0; i < data.length; i++) {
+        const badge = data[i] as IUserBadge;
+        for (let j = 0; j < ge.length; j++) {
+            const item = ge[j];
+            if (item.id === badge.name) {
+                try {
+                    const version = await item.getVersion(badge.version);
+                    const url = await version?.getImageUrl(1);
+                    badge.imgUrl = url;
+                    resolvedBadges.push(badge);
+                    break;
+                }
+                catch (error) {
+                    res.status(500).send('Badge API calls failed');
+                }
+            }
+        }
+    }
+
+    if (resolvedBadges.length > 0) {
+        res.status(200).json(resolvedBadges);
+    }
+    else {
+        res.status(201).send('No Data');
     }
 });
 
